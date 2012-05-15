13cc09cfc4d247703b15bdfb00dae9d50e9b2c0d
Evan Kerrigan <apps@evankerrigan.com>
2012-05-15 17:01:14 -0400

    Fixed lighting issue: power down other color light when activiting a particular colored light

--- lab/mic_test/mic_test.ino
+++ lab/mic_test/mic_test.ino
@@ -1,38 +1,34 @@
-int yPin = 9;
-int rPin = 13;
-int micPin = 0;
-int micValue = 0;
+int val;
 
-int lowThresh = 500;
-int medThresh = 600;
-int highThresh = 1000;
+int audioPort = 0;
+int yellowLed = 4;
+int redLed = 7;
 
+int lowThreshold = 780;
+int mediumThreshold = 600;
+int highThreshold = 900;
 
 void setup() {
-  pinMode(yPin, OUTPUT);
-  pinMode(rPin, OUTPUT);  
-  //Serial.begin(9600);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(redLed, OUTPUT);  
+  Serial.begin(9600);
 }
 
 void loop() {
-  int micLevel = analogRead(micPin);
-  //Serial.println(micLevel, DEC);     // prints the value read
- /*
-  micLevel = map(micLevel, 0, 1023, 0, 255);
-  micLevel = constrain(micLevel, 0, 255);
-  analogWrite(ledPin, micLevel);
-  delay(100);                        // wait 100ms for next reading
-  */
-  
-  if (micLevel > medThresh) {
-    digitalWrite(yPin, HIGH);
-    digitalWrite(rPin, LOW);    
-    delay(10);
-    digitalWrite(yPin, LOW);
-  } else if (micLevel > lowThresh) {
-    digitalWrite(rPin, HIGH);
-    digitalWrite(yPin, LOW);
-    delay(10);
-    digitalWrite(rPin, LOW);
+  val = analogRead(audioPort);
+  Serial.println(val, DEC);     // prints the value read
+  if(val > highThreshold) { 
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(redLed, HIGH);
+    delay(10);              
+    digitalWrite(redLed, LOW); 
+  } 
+  else if(val > mediumThreshold) {
+    digitalWrite(redLed, LOW);
+    digitalWrite(yellowLed, HIGH);
+    delay(10);              
+    digitalWrite(yellowLed, LOW); 
   }
 }
+
+
