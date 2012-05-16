07039e689be1538fbafbb4811b5910bdbebac46b
Evan Kerrigan <apps@evankerrigan.com>
2012-05-16 14:18:18 -0400

    Multiple light volume input LED viz

---
+++ lab/multi_mic_test/multi_mic_test.ino
@@ -0,0 +1,39 @@
+int ledPin[] = {\2, 4, 7, 8};
+int currentLevel = 0;
+
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPin[i], OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  int mic = analogRead(A0);
+  Serial.println(mic, DEC);     // prints the value read
+  int prevLevel = currentLevel;
+
+  if (mic < 500) {
+    mic = 0;
+  } 
+  else { 
+    mic = mic - 500;
+  }
+
+
+  currentLevel = map(mic, 0, 523, 0, 3);
+
+  if (prevLevel > currentLevel) {
+    for (int i = prevLevel; i >= currentLevel; i--) {
+      digitalWrite(ledPin[i], LOW);
+    }
+  } else if (currentLevel > prevLevel) {
+  for (int i = prevLevel; i <= currentLevel; i++) {
+    digitalWrite(ledPin[i], HIGH);
+  }
+}
+}
+
+
+
+
