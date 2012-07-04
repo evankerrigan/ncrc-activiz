5abe026593f42153dd46449e91913c2bc8a07e44
sprite728 <cchuang@server>
2012-07-04 17:41:18 -0400

    Delete libraries-copy

--- libraries-copy/NcrcViz/NcrcViz.h
+++
@@ -1,4 +0,0 @@
-#pragma once 
-
-#include "NcrcVizConfig.h"
-#include "SoundHandler.h"
--- libraries-copy/ledcontroller/python/ledcontroller/patterns/Manifest.py
+++
@@ -1,5 +0,0 @@
-from Pattern import Pattern
-from PatternList import PatternList
-from InterpolatedMarquee import InterpolatedMarquee
-from Pulser import Pulser
-from ObscuringColor import ObscuringColor
