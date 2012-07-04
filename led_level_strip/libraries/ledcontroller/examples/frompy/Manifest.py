19c128c15a6fad34da16e8b428a8d9391e677785
sprite728 <cchuang@server>
2012-07-04 17:39:35 -0400

    Delete duplicated files

--- led_level_strip/libraries/ledcontroller/examples/frompy/Manifest.py
+++
@@ -1,8 +0,0 @@
-# Set up paths to include the ledcontroller Python library.
-import os, sys
-ledControllerLibPath = os.path.abspath(
-	os.path.join(os.path.dirname(__file__), '..', '..', 'python'))
-sys.path.append(ledControllerLibPath)
-
-import ledcontroller
-
--- ncrc_activiz/ncrcviz.h
+++
@@ -1,4 +0,0 @@
-#pragma once 
-
-#include "Config.h"
-#include "EventHandler.h"
